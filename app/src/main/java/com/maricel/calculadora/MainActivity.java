4a7aa549248aaf48644693687bf0854164489d32
Maricel1997 <maricelriveros19@gmail.com>
2023-10-20 20:51:32 -0300

    Implementacion Finalizada actualizado

--- app/src/main/java/com/maricel/calculadora/MainActivity.java
+++ app/src/main/java/com/maricel/calculadora/MainActivity.java
@@ -10,12 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText val1, val2;
-    Button btnSumar, btnRestar, btnDividir, btnMult;
+    Button btnSumar, btnRestar, btnDividir, btnMultiplicar;
 
     TextView resultado;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
         val1 = findViewById(R.id.primerVal);
@@ -23,61 +22,46 @@ public class MainActivity extends AppCompatActivity {
         btnSumar = findViewById(R.id.btnSumar);
         btnRestar = findViewById(R.id.btnRestar);
         btnDividir = findViewById(R.id.btnDividir);
-        btnMult = findViewById(R.id.btnMult);
+        btnMultiplicar = findViewById(R.id.btnMult);
         resultado = findViewById(R.id.resultado);
-
-
     }
 
-    public void sumar(View view) {
-
+    public void sumar(View view){
         String primerVal = val1.getText().toString();
         String segundoVal = val2.getText().toString();
-
         double v1 = Double.parseDouble(primerVal);
         double v2 = Double.parseDouble(segundoVal);
-        resultado.setText("" + (v1 + v2));
-
+        resultado.setText(""+(v1+v2));
     }
 
-    public void restar(View view) {
-
+    public void restar(View view){
         String primerVal = val1.getText().toString();
         String segundoVal = val2.getText().toString();
-
         double v1 = Double.parseDouble(primerVal);
         double v2 = Double.parseDouble(segundoVal);
-        resultado.setText("" + (v1 - v2));
-
+        resultado.setText(""+(v1-v2));
     }
 
-    public void dividir(View view) {
-
+    public void dividir(View view){
         String primerVal = val1.getText().toString();
         String segundoVal = val2.getText().toString();
-
         double v1 = Double.parseDouble(primerVal);
         double v2 = Double.parseDouble(segundoVal);
-        resultado.setText("" + (v1 / v2));
+        resultado.setText(""+(v1/v2));
     }
 
-    public void multiplicar(View view) {
-
+    public void multiplicar(View view){
         String primerVal = val1.getText().toString();
         String segundoVal = val2.getText().toString();
-
         double v1 = Double.parseDouble(primerVal);
         double v2 = Double.parseDouble(segundoVal);
-        resultado.setText("" + (v1 * v2));
-
+        resultado.setText(""+(v1*v2));
     }
-    public void restear(View view) {
+
+    public void resetear(View view){
         val1.setText(null);
         val2.setText(null);
         resultado.setText(null);
-
-
     }
+
 }
-
-
